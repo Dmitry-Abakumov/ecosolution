4ae11759a4e4db058b316014b75c47c6cfecbc68
Dmitry Abakumov <dm.abakumov@gmail.com>
2023-12-06 13:14:57 +0200

    Change logo`s link path

--- src/schared/components/Logo/Logo.jsx
+++ src/schared/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import css from "./Logo.module.css";
 
 const Logo = () => {
   return (
-    <a href="../../../../public/index.html" className={css.logo}>
+    <a href="/index.html" className={css.logo}>
       <Icon iconName="logo" width="31" height="18" />
       <span className={css.title}>ecosolution</span>
       <span className={css.slogan}>
